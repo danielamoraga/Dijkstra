5db2a94338d4bd4949e4ab29bac0e0d104fb6b1d
elegoose <fel.torra@gmail.com>
2024-06-20 14:37:10 -0400

    copied fibheap_gfg.hpp code into structures

--- structures.hpp
+++ structures.hpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+using element = pair<double, int>;  // (distance, node)
 
 const double infinity = numeric_limits<double>::infinity();
 const int undefined = numeric_limits<int>::quiet_NaN();
@@ -19,7 +20,6 @@ struct graph {
 
 /* Heap */
 struct heap {
-    using element = pair<double, int>;  // (distance, node)
     set<element> s;
     unordered_map<int, double> pos;  // node -> distance
 
@@ -56,261 +56,267 @@ struct heap {
     }
 };
 
-
-/*  Fibonacci heap: collection of rooted trees that are min-heap ordered
-    (each tree obeys the min-heap property: the key of a node is greater than or equal to the key of its parent) */
-
 // Creating a structure to represent a node in the heap
 struct node {
-    using element = pair<double, int>;
-    node* p; // pointer to parent
-	node* child; // pointer to any children (they're linked together in a circular doubly linked list -> child list)
-	node* left; // pointer to the sibling node on the left of a child
-	node* right; // pointer to the sibling on the right of a child
-    // if a node it's an only child: y.left = y.right = y
-	element key; // Value of the node
-	int degree; // number of children in the child list
-	bool mark; // true if node x lost a child since the last time the node was made child of another node
-    // newly created nodes are unmarked and a node becomes unmarked whenever it is made the child of another node
-    node* min; // pointer to the root of a tree containing the minimum key
-    int n; // number of nodes
-    node* rootList; // circular doubly linked list of all tree roots in the heap
-
-    unordered_map<int, node*> pos;
-
-    node() {min = NULL; n = 0; rootList = NULL;}
-
-    private:
-        /* operaciones de listas circulares doblemente enlazadas */
-        // crear lista vacía
-        void clearList(node* &L) {
-            L = NULL;
-            return;
-        }
-        // insertar elemento en una lista
-        void insertList(node* &L, node* x) {
-            if (L == NULL) { // si la lista está vacía
-                L = x; // pasa a tener solo el elemento insertado
-                L->left = L->right = x;
-                return;
-            } else {
-                (L->left)->right = x;
-                x->right = L;
-                x->left = L->left;
-                L->left = x;
-            }
-        }
-        // concatenar dos listas
-        node* concatenateList(node* &L1, node* L2) {
-            if (L1 == NULL) return L2;
-            if (L2 == NULL) return L1;
-
-            node *next1 = L1->right;
-            node *next2 = L2->left;
-
-            L2->left = L1;
-            L1->right = L2;
-            next2->left = next1;
-            next1->right = next2;
-
-            return L1;
-        }
-        // quitar un nodo de una lista
-        void removeList(node* &L, node* x) {
-            node* curr = L->right;
-            if (L == NULL || x == NULL) return;
-            if (L == x) L = x->right;
-            else {
-                while (curr != L) {
-                    if (curr == x) {
-                        curr->right->left = curr->left;
-                        curr->left->right = curr->right;
-                        break;
-                    }
-                    curr = curr->right;
-                }
-            }
-            return;
-        }
-
-        /* operaciones de árboles binomiales */
-        // hacer que y sea un hijo de x
-        void makeChild(node* x, node* y){
-            if (x->child == NULL) {
-                x->child = y;
-            } else {
-                insertList(x->child, y);
-            }
-            x->degree++;
-        }
-        // quitar x de la lista de hijos de y
-        void removeChild(node* x, node* y) {
-            removeList(y->child,x);
-            y->degree--;
-        }
-
-        /* funciones auxiliares */
-        // make an empty fibonacci heap
-        node* makefibheap() {
-            node* H = new node();
-            H->n = 0;
-            H->min = NULL; // when a fibheap is empty, min is NULL (default)
-            // there are no trees in H
-            return H;
-        }
-        // mergeHeaps
-        node* mergeHeaps(node* H1, node* H2) {
-            node* H = makefibheap();
-            H->min = H1->min;
-            H->rootList = concatenateList(H2->rootList,H->rootList);
-            if((H1->min == NULL) || (H2->min != NULL && H2->min->key.first < H1->min->key.first))
-                H->min = H2->min;
-            H->n = H1->n + H2->n;
-            return H;
-        }
-        void fibheaplink(node* H, node* y, node* x) {
-            removeList(H->rootList, y); // remove y from the root list of H
-            makeChild(x,y); // make y a child of x, incrementing x.degree
-            y->mark = false; // y.mark = false
-        }
-        // logaritmo en base phi de n
-        double log_phi(int n) {
-            double phi = (1 + sqrt(5)) / 2; // golden ratio
-            double res = log(n) / log(phi);
-            return res;
-        }
-        // calculate upper bound D(H->n) on the maxximum degree
-        int upper_bound() {
-            int D_n = floor(log_phi(n));
-            return D_n;
-        }
-        void consolidate() {
-            node* A[upper_bound()];
-            for (int i=0; i<upper_bound(); i++) {
-                A[i] = NULL;
-            }
-            node* w = rootList->right;
-            while (w != rootList) {
-                node* x = w;
-                int d = x->degree;
-                while (A[d] != NULL) {
-                    node* y = A[d];
-                    if (x->key.first > y->key.first)
-                        swap(x,y);
-                    fibheaplink(this,y,x);
-                    A[d] = NULL;
-                    d++;
-                }
-                A[d] = x;
-                w = w->right;
-            }
-            min = NULL;
-            for (int i=0; i<upper_bound(); i++) {
-                if (A[i] != NULL) {
-                    if (min == NULL) {
-                        clearList(rootList); // create a root list for H containing just A[i]
-                        insertList(rootList, A[i]);
-                        min = A[i];
-                    } else {
-                        insertList(rootList, A[i]);
-                        if (A[i]->key.first < min->key.first)
-                            min = A[i];
-                    }
-                }
-            }
-        }
-        void cut(node* H, node* x, node* y) {
-            removeChild(x, y); // remove x from the child list of y, decrementing y.degree
-            insertList(H->rootList, x);
-            x->p = NULL;
-            x->mark = false;
-        }
-        void cascading_cut(node* H, node* y) {
-            node* z = y->p;
-            if (z != NULL) {
-                if (y->mark == false)
-                    y->mark = true;
-                else {
-                    cut(H,y,z);
-                    cascading_cut(H,z);
-                }
-            }
-        }
-        // void deletion(node* x) {
-        //     decreaseKey(x,-INFINITY);
-        //     extract();
-        // }
-
-    public:
-        // inserting a node
-        void insert(element e) {
-            node* x = new node();
-            x->key = e;
-            x->degree = 0;
-            x->p = NULL;
-            x->child = NULL;
-            x->left = x;
-            x->right = x;
-            x->mark = false;
-            if (min == NULL) {
-                rootList = x;
-                min = x;
-            } else {
-                insertList(rootList, x);
-                if (x->key.first < min->key.first)
-                    min = x;
-            }
-            n++;
-
-            pos[e.second] = x;
-        }
-
-        // find minimum
-        element find() {
-            return min->key;
-        }
-
-        // extract min
-        void extract() {
-            node* z = min;
-            if (z != NULL) {
-                if (z->child != NULL) {
-                    node* x = z->child->right;
-                    cout << "while extract!!" << endl;
-                    while (x != z->child) { // for each child x of z
-                        insertList(rootList, x); // add x to the root list of H
-                        x->p = NULL;
-                        x = x->right;
-                    }
-                }
-                cout << "remove list!!" << endl;
-                removeList(rootList, z); // remove z from the root list of H
-                if (z == z->right)
-                    min = NULL;
-                else {
-                    min = z->right;
-                    consolidate();
-                }
-                n--;
-            }
-            pos.erase(z->key.second);
-            return;
-        }
-
-        // void decreaseKey(double p, int u)
-        void decreaseKey(double p, int u) {
-            node* x = pos[u];
-            x->key.first = p;
-            node* y = x->p;
-            if (y != NULL && x->key.first < y->key.first) {
-                cut(this,x,y);
-                cascading_cut(this,y);
-            }
-            if (x->key.first < min->key.first)
-                min = x;
-        }
-
-        bool isEmpty() {
-            return n==0;
-        }
+    node* parent;  // Parent pointer
+    node* child;   // Child pointer
+    node* left;    // Pointer to the node on the left
+    node* right;   // Pointer to the node on the right
+    element key;   // Value of the node
+    int degree;    // Degree of the node
+    char mark;     // Black or white mark of the node
+    char c;        // Flag for assisting in the Find node function
 };
+
+struct fibheap {
+    // Creating min pointer as "mini"
+    node* mini = NULL;
+    // Declare an integer for number of nodes in the heap
+    int no_of_nodes = 0;
+
+    // Function to insert a node in heap
+   public:
+    void insert(element val) {
+        struct node* new_node = new node();
+        new_node->key = val;
+        new_node->degree = 0;
+        new_node->mark = 'W';
+        new_node->c = 'N';
+        new_node->parent = NULL;
+        new_node->child = NULL;
+        new_node->left = new_node;
+        new_node->right = new_node;
+        if (mini != NULL) {
+            (mini->left)->right = new_node;
+            new_node->right = mini;
+            new_node->left = mini->left;
+            mini->left = new_node;
+            if (new_node->key.first < mini->key.first)
+                mini = new_node;
+        } else {
+            mini = new_node;
+        }
+        no_of_nodes++;
+    }
+    // Finding the minimum distance value node
+   public:
+    element find() {
+        return mini->key;
+    }
+    // Linking the heap nodes in parent child relationship
+   private:
+    void fiblink(struct node* ptr2, struct node* ptr1) {
+        (ptr2->left)->right = ptr2->right;
+        (ptr2->right)->left = ptr2->left;
+        if (ptr1->right == ptr1)
+            mini = ptr1;
+        ptr2->left = ptr2;
+        ptr2->right = ptr2;
+        ptr2->parent = ptr1;
+        if (ptr1->child == NULL)
+            ptr1->child = ptr2;
+        ptr2->right = ptr1->child;
+        ptr2->left = (ptr1->child)->left;
+        ((ptr1->child)->left)->right = ptr2;
+        (ptr1->child)->left = ptr2;
+        if (ptr2->key.first < (ptr1->child)->key.first)
+            ptr1->child = ptr2;
+        ptr1->degree++;
+    }
+    // Consolidating the heap
+   private:
+    void consolidate() {
+        int temp1;
+        float temp2 = (log(no_of_nodes)) / (log(2));
+        int temp3 = temp2;
+        struct node* arr[temp3 + 1];
+        for (int i = 0; i <= temp3; i++)
+            arr[i] = NULL;
+        node* ptr1 = mini;
+        node* ptr2;
+        node* ptr3;
+        node* ptr4 = ptr1;
+        do {
+            ptr4 = ptr4->right;
+            temp1 = ptr1->degree;
+            while (arr[temp1] != NULL) {
+                ptr2 = arr[temp1];
+                if (ptr1->key.first > ptr2->key.first) {
+                    ptr3 = ptr1;
+                    ptr1 = ptr2;
+                    ptr2 = ptr3;
+                }
+                if (ptr2 == mini)
+                    mini = ptr1;
+                fiblink(ptr2, ptr1);
+                if (ptr1->right == ptr1)
+                    mini = ptr1;
+                arr[temp1] = NULL;
+                temp1++;
+            }
+            arr[temp1] = ptr1;
+            ptr1 = ptr1->right;
+        } while (ptr1 != mini);
+        mini = NULL;
+        for (int j = 0; j <= temp3; j++) {
+            if (arr[j] != NULL) {
+                arr[j]->left = arr[j];
+                arr[j]->right = arr[j];
+                if (mini != NULL) {
+                    (mini->left)->right = arr[j];
+                    arr[j]->right = mini;
+                    arr[j]->left = mini->left;
+                    mini->left = arr[j];
+                    if (arr[j]->key.first < mini->key.first)
+                        mini = arr[j];
+                } else {
+                    mini = arr[j];
+                }
+                if (mini == NULL)
+                    mini = arr[j];
+                else if (arr[j]->key.first < mini->key.first)
+                    mini = arr[j];
+            }
+        }
+    }
+    // Function to extract minimum node in the heap
+   public:
+    void extract() {
+        if (mini == NULL)
+            cout << "The heap is empty" << endl;
+        else {
+            node* temp = mini;
+            node* pntr;
+            pntr = temp;
+            node* x = NULL;
+            if (temp->child != NULL) {
+                x = temp->child;
+                do {
+                    pntr = x->right;
+                    (mini->left)->right = x;
+                    x->right = mini;
+                    x->left = mini->left;
+                    mini->left = x;
+                    if (x->key.first < mini->key.first)
+                        mini = x;
+                    x->parent = NULL;
+                    x = pntr;
+                } while (pntr != temp->child);
+            }
+            (temp->left)->right = temp->right;
+            (temp->right)->left = temp->left;
+            mini = temp->right;
+            if (temp == temp->right && temp->child == NULL)
+                mini = NULL;
+            else {
+                mini = temp->right;
+                consolidate();
+            }
+            no_of_nodes--;
+        }
+    }
+    // Cutting a node in the heap to be placed in the root list
+   private:
+    void cut(struct node* found, struct node* temp) {
+        if (found == found->right)
+            temp->child = NULL;
+
+        (found->left)->right = found->right;
+        (found->right)->left = found->left;
+        if (found == temp->child)
+            temp->child = found->right;
+
+        temp->degree = temp->degree - 1;
+        found->right = found;
+        found->left = found;
+        (mini->left)->right = found;
+        found->right = mini;
+        found->left = mini->left;
+        mini->left = found;
+        found->parent = NULL;
+        found->mark = 'B';
+    }
+    // Recursive cascade cutting function
+   private:
+    void cascade_cut(struct node* temp) {
+        node* ptr5 = temp->parent;
+        if (ptr5 != NULL) {
+            if (temp->mark == 'W') {
+                temp->mark = 'B';
+            } else {
+                cut(temp, ptr5);
+                cascade_cut(ptr5);
+            }
+        }
+    }
+    // Function to decrease the value of a node in the heap
+   private:
+    void decrease_key(struct node* found, double val) {
+        if (mini == NULL)
+            cout << "The Heap is Empty" << endl;
+
+        if (found == NULL)
+            cout << "Node not found in the Heap" << endl;
+
+        found->key.first = val;
+
+        struct node* temp = found->parent;
+        if (temp != NULL && found->key.first < temp->key.first) {
+            cut(found, temp);
+            cascade_cut(temp);
+        }
+        if (found->key.first < mini->key.first)
+            mini = found;
+    }
+    // Function to find the given node
+   private:
+    void find(struct node* mini, int old_val, double val) {
+        struct node* found = NULL;
+        node* temp5 = mini;
+        temp5->c = 'Y';
+        node* found_ptr = NULL;
+        if (temp5->key.second == old_val) {
+            found_ptr = temp5;
+            temp5->c = 'N';
+            found = found_ptr;
+            decrease_key(found, val);
+        }
+        if (found_ptr == NULL) {
+            if (temp5->child != NULL)
+                find(temp5->child, old_val, val);
+            if ((temp5->right)->c != 'Y')
+                find(temp5->right, old_val, val);
+        }
+        temp5->c = 'N';
+        found = found_ptr;
+    }
+    // Decreasing the distance key if node with key u
+   public:
+    void decreaseKey(double p, int u) {
+        find(mini, u, p);
+    }
+    // Deleting a node from the heap
+   private:
+    void deletion(int val) {
+        if (mini == NULL)
+            cout << "The heap is empty" << endl;
+        else {
+            // Decreasing the value of the node to 0
+            find(mini, val, 0);
+            // Calling Extract_min function to
+            // delete minimum value node, which is 0
+            extract();
+            cout << "Key Deleted" << endl;
+        }
+    }
+    // See if a fibheap is empty
+   public:
+    bool isEmpty() {
+        if (mini == NULL)
+            return true;
+        else
+            return false;
+    }
+};
+
+// Fuente: https://www.geeksforgeeks.org/fibonacci-heap-deletion-extract-min-and-decrease-key/
